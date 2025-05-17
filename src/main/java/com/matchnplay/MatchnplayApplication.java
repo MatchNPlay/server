b282c3a570bf1578275f07079b1511ee1bc4f02d
suheda-snr <t3sesu00@students.oamk.fi>
2025-05-17 17:24:27 +0300

    first commit

---
+++ src/main/java/com/matchnplay/MatchnplayApplication.java
@@ -0,0 +1,13 @@
+package com.matchnplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchnplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchnplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matchnplay/MatchnplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matchnplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchnplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
