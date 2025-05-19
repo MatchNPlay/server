98080276e9d1726ce7e349b9f3f13c4edce8c348
suheda-snr <t3sesu00@students.oamk.fi>
2025-05-20 00:13:31 +0300

    update password validation regex to allow additional special characters

--- src/main/java/com/matchnplay/dto/auth/RegistrationRequestDto.java
+++ src/main/java/com/matchnplay/dto/auth/RegistrationRequestDto.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.Pattern;
 public record RegistrationRequestDto(
         @NotBlank String username,
         @Email @NotBlank String email,
-        @NotBlank @Size(min = 8, message = "Password must be at least 8 characters") @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*]).+$", message = "Password must contain uppercase, lowercase, digit, and special character") String password,
+        @NotBlank @Size(min = 8, message = "Password must be at least 8 characters") @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*._-]).+$", message = "Password must contain uppercase, lowercase, digit, and special character") String password,
         @NotBlank String fullname,
         @NotBlank String phone) {
 }
